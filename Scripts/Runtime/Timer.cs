57da36a40e5b348a8b4db431fddd7808e56b89e7
Eduardo Pons <eduardo.costa@toptal.com>
2020-08-06 20:09:24 -0300

    Timer: Created the timer class as an extension of Activity

---
+++ Scripts/Runtime/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExt.Core {
+
+    /// <summary>
+    /// Class that extends an Activity to give support of time tracking, laps and delayed execution start.
+    /// </summary>
+    public class Timer : Activity {
+        
+    }
+
+}
